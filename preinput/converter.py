8d7885a8d58ac90e4b965ef478c62788f7d40032
Sucan <qz2280@columbia.edu>
2017-08-09 21:14:13 -0400

    Big change to converter.py

--- preinput/converter.py
+++ preinput/converter.py
@@ -9,17 +9,25 @@ class UnitConverter:
     it will automatically transform the first in terms of the second one.
     """
 
-    def __init__(self):
+    @staticmethod
+    def simplest_converter(scale, num, from_unit, to_unit):
         """
-        Define some SI unit constants.
-        """
-        self.bohr_radius = 5.2917721067e-11
-        self.hartree_energy = 4.359744650e-18
-        self.electron_volt = 1.602176565e-19
-        self.boltzmann_const = 1.38064852e-23
-        self.avogadro_const = 6.022140857e23
 
-    def length_converter(self, num, from_unit='a', to_unit='b'):
+        :param scale:
+        :param num:
+        :param from_unit:
+        :param to_unit:
+        :return:
+        """
+        normalized_num = num * scale.get(from_unit)
+        return normalized_num / scale.get(to_unit)
+
+
+class LengthConverter(UnitConverter):
+    def __init__(self):
+        self.bohr_radius = 5.2917721067e-11
+
+    def simple_converter(self, num, from_unit='a', to_unit='b'):
         """
         This function converts the input first to meter, then converts it to desired unit.
         :param num: float
@@ -32,18 +40,21 @@ class UnitConverter:
         scale.update(dict.fromkeys(['a', 'angstrom', 'A'], 1e-10))
         scale.update(dict.fromkeys(['cm', 'centimeter'], 1e-2))
         scale.update(dict.fromkeys(['nm', 'nanometer'], 1e-9))
-        scale.update(dict.fromkeys(
-            ['b', 'bohr', 'au', 'atomic'], self.bohr_radius))
-        normalized_num = num * scale.get(from_unit)
-        return normalized_num / scale.get(to_unit)
+        scale.update(dict.fromkeys(['b', 'bohr', 'au', 'atomic'], self.bohr_radius))
+        return self.simplest_converter(scale, num, from_unit, to_unit)
 
-    def volume_converter(self, num, from_unit='a3', to_unit='b3'):
+
+class VolumeConverter(UnitConverter):
+    def __init__(self):
+        self.bohr_radius = 5.2917721067e-11
+
+    def simple_converter(self, num, from_unit='a3', to_unit='b3'):
         """
         This function converts the input first to cubic meter, then converts it to desired unit.
-        :param num: float
-        :param from_unit: str
-        :param to_unit: str
-        :return: float
+        :param num:
+        :param from_unit:
+        :param to_unit:
+        :return:
         """
         scale = dict.fromkeys(['m3', 'cubicmeter'], 1)
         scale.update(dict.fromkeys(['cm3', 'cubiccentimeter'], 1e-6))
@@ -51,16 +62,22 @@ class UnitConverter:
         scale.update(dict.fromkeys(['a3', 'cubicangstrom'], 1e-30))
         scale.update(dict.fromkeys(
             ['b3', 'cubicbohr', 'au3', 'cubicatomic'], self.bohr_radius ** 3))
-        normalized_num = num * scale.get(from_unit)
-        return normalized_num / scale.get(to_unit)
+        return self.simplest_converter(scale, num, from_unit, to_unit)
 
-    def energy_converter(self, num, from_unit='ha', to_unit='ry'):
+
+class EnergyConverter(UnitConverter):
+    def __init__(self):
+        self.hartree_energy = 4.359744650e-18
+        self.electron_volt = 1.602176565e-19
+        self.boltzmann_const = 1.38064852e-23
+
+    def simple_converter(self, num, from_unit='ha', to_unit='ry'):
         """
         This function converts the input first to Joule, then converts it to desired unit.
-        :param num: float
-        :param from_unit: str
-        :param to_unit: str
-        :return: float
+        :param num:
+        :param from_unit:
+        :param to_unit:
+        :return:
         """
         scale = dict.fromkeys(['J', 'Joule', 'SI'],
                               1)  # Joule is the standard unit.
@@ -70,11 +87,11 @@ class UnitConverter:
             ['ev', 'eV', 'electronvolt'], self.electron_volt))
         scale.update(dict.fromkeys(['ry', 'rydberg'], self.hartree_energy / 2))
         scale.update(dict.fromkeys(['K'], self.boltzmann_const))
-        normalized_num = num * scale.get(from_unit)
-        return normalized_num / scale.get(to_unit)
+        return self.simplest_converter(scale, num, from_unit, to_unit)
 
-    @staticmethod
-    def pressure_converter(num, from_unit='gpa', to_unit='mbar'):
+
+class PressureConverter(UnitConverter):
+    def simple_converter(self, num, from_unit='gpa', to_unit='mbar'):
         """
         This function converts the input first to pascal, then converts it to desired unit.
         :param num: float
@@ -87,10 +104,14 @@ class UnitConverter:
         scale.update(dict.fromkeys(['gpa', 'GPa'], 1e9))
         scale.update(dict.fromkeys(['bar'], 1e5))
         scale.update(dict.fromkeys(['mbar', 'Mbar'], 1e11))
-        normalized_num = num * scale.get(from_unit)
-        return normalized_num / scale.get(to_unit)
+        return self.simplest_converter(scale, num, from_unit, to_unit)
 
-    def mole_converter(self, option, num, from_unit, to_unit):
+
+class MoleConverter(UnitConverter):
+    def __init__(self):
+        self.avogadro_const = 6.022140857e23
+
+    def converter(self, option, num, from_unit, to_unit):
         """
         Sometimes we meet a quantity with xx/mol, this converts it to the physical quantity of only one such quantity.
         Supported options: {'length', 'volume', 'energy', 'pressure'}
@@ -100,15 +121,28 @@ class UnitConverter:
         :param to_unit: str
         :return: float
         """
-        options = {'length': self.length_converter, 'volume': self.volume_converter, 'energy': self.energy_converter,
-                   'pressure': self.pressure_converter}
+        options = {'l': LengthConverter, 'v': VolumeConverter, 'e': EnergyConverter,
+                   'p': PressureConverter}
         return options[option](num / self.avogadro_const, from_unit, to_unit)
 
 
+def simple_converter(physical_quantity, num, from_unit, to_unit):
+    """
+    This is a polymorphism function, it can apply to any object that defined simple_converter method.
+    :param physical_quantity:
+    :param num:
+    :param from_unit:
+    :param to_unit:
+    :return:
+    """
+    unitconverters = {'l': LengthConverter, 'v': VolumeConverter, 'e': EnergyConverter,
+                      'p': PressureConverter}
+    unitconverter = unitconverters[physical_quantity]()
+    return unitconverter.simple_converter(num, from_unit, to_unit)
+
+
 if __name__ == '__main__':
-    uc = UnitConverter()
-    # print(uc.length_converter(7.4268, 'b', 'a'))
-    # print(uc.volume_converter(11.176, 'a3', 'b3'))
-    print(uc.energy_converter(2000, 'K', 'ry'))
-    # print(uc.pressure_converter(3, 'mbar', 'gpa'))
-    # print(uc.mole_converter('volume', 6.73, 'cm3', 'au3'))
+    print(simple_converter('l', 7.4268, 'b', 'a'))
+    print(simple_converter('v', 11.176, 'a3', 'b3'))
+    print(simple_converter('e', 2000, 'K', 'ry'))
+    print(simple_converter('p', 3, 'mbar', 'gpa'))
