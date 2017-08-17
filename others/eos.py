7d971ddac4ba50b7e6e15cea97f0f76552c0937b
Sucan <qz2280@columbia.edu>
2017-08-17 13:55:36 -0400

    Improve converter.py

---
+++ others/eos.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# created at Jul 19, 2017 16:35 by Nil-Zil
+
+import numpy as np
+from scipy.optimize import fsolve
+
+
+class EOS:
+    def vinet(self, v: float, v0: float, k0: float, k0p: float) -> float:
+        """
+        Vinet EOS takes 3 parameters to form a function P of V.
+        :param v: float
+        :param v0: float
+        :param k0: float
+        :param k0p: float
+        :return: float
+        """
+        r = (v / v0) ** (1 / 3)
+        return 3 * k0 * r ** (-2) * (1 - r) * np.exp(3 / 2 * (k0p - 1) * (1 - r))
+
+    def solve_vinet(self, p: float, v0: float, k0: float, k0p: float) -> tuple:
+        """
+
+        :param p:
+        :param v0: float
+        :param k0: float
+        :param k0p: float
+        :return: (ndarray, dict, int, str)
+        """
+
+        def func(v):
+            return self.vinet(v, v0, k0, k0p) - p
+
+        return fsolve(func, np.array([100]))
+
+
+if __name__ == "__main__":
+    eos = EOS()
+    print(eos.vinet(100, 137.6852, 283.29, 4.86))
+    print(eos.solve_vinet(0, 137.6852, 283.29, 4.86))
---
+++ others/eos_test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# created at Aug 17, 2017 1:02 PM by Nil-Zil
+"""
+This is a unit test module of eos.py.
+If all tests are OK, it does not mean that the program has no bug.
+But if unittest does not pass, there must be bug(s) in the program.
+"""
+
+import unittest
+
+import numpy as np
+
+from .eos import EOS
+
+
+class TestEOS(unittest.TestCase):
+    def setUp(self):
+        self.eos = EOS()
+
+    def test_vinet(self):
+        self.assertAlmostEqual(self.eos.vinet(
+            100, 137.6852, 283.29, 4.86), 190.99334615)
+
+    def test_solve_vinet(self):
+        self.assertAlmostEqual(self.eos.solve_vinet(0, 137.6852, 283.29, 4.86)[0], 137.6852)
+
+
+if __name__ == "__main__":
+    unittest.main()
