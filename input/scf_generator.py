40fa4836f53cb4885092ee4128062db63ca76cd2
Sucan <qz2280@columbia.edu>
2017-10-19 20:06:14 -0400

    Modify scf generation part

--- input/scf_generator.py
+++ input/scf_generator.py
@@ -1,27 +1,58 @@
 #!/usr/bin/env python3
-# created at Jul 21, 2017 12:29 by Nil-Zil
+# created at Jul 21, 2017 12:29 by Qi Zhang
 
-import re
+from typing import *
+from input.pw_paramlist import *
 
-
-# TODO: Separate by :, [], ! comment
 
 class SCFGenerator:
-    def __init__(self):
-        pass
+    def __init__(self, output_name: str, calc_type: str):
+        self.scf_output = output_name
+        self.calc_type = calc_type
 
-    def readfile(self, file):
+    @staticmethod
+    def readfile(file) -> Dict:
+        """
+        This method reads input from a given template file. Each line in the file is of form:
+            key: value
+        or
+            key: [value1, value2, ...]
+        Then the method generates a dictionary that contains each different keys and corresponding
+        value(s).
+
+        :param file: the template file to be read from
+        :return: A dictionary that contains each pair of key and value(s) on each line in the file.
+        """
+        param_dict = {}
+
         with open(file, 'r') as f:
-            lines = f.readlines()
-            for i in range(len(lines)):
-                kv = lines[i].split('=')
-                k = kv[0]
-                v = kv[1]
-                if '!' in v:
-                    v = v.split('!')[0]  # ignore things after the first '!'
-                v = v.split(':')
-                v = list(map(lambda x: x.strip(), v))
+            for line in f:
+                stripped_line = line.strip()
+                # If a line starts with '#', it will be regarded as a comment,
+                # we do not parse this line.
+                if not stripped_line.startswith('#'):
+                    continue
+                if stripped_line:  # If this line is not a blank line
+                    continue
+                # Use ':' as the delimiter, split the line into key and value.
+                k, v = stripped_line.split(':', maxsplit=1)
+                k: str = k.strip()
+                v: str = v.strip()
+                if not k:  # If k is ''
+                    raise ValueError('Key is empty string!')
+                if not v:  # If v is ''
+                    raise ValueError('Value is empty string!')
 
+                if v.startswith('[') and v.endswith(']'):  # v is a list
+                    vs = v[1:-2].split(',')  # Skip '[' and ']', then split by ','
+                    vx = [x.strip() for x in vs]
+                else:
+                    vx = v
+                param_dict.update({k: vx})
 
+        return param_dict
 
-
+    def construct_CONTROL(self):
+        with open(self.scf_output, 'w') as f:
+            f.write("&CONTROL\n")
+            f.write("calculation = '%s'\n".format())
